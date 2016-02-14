4ef5e2128c24b3a65049053fb721a1ed8e2b39d1
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-02-14 12:28:30 -0800

    Tests

--- stack.php
+++ stack.php
@@ -28,7 +28,9 @@ $obj = json_decode($result,true);
 //--print_r($obj); //--uncomment for debug
 
 foreach ($obj["items"] as $value){
-	echo "<div class='result_case'><div style='background:url(\"https://twiicdn.com/imgcache/?url=".urlencode($value["owner"]["profile_image"])."&size=100\");background-size: cover;background-position: 50% 50%;' class='result_image'></div></div>";
+	if ($value["owner"]["user_type"]=="registered"){
+		echo "<div class='result_case'><div style='background:url(\"https://twiicdn.com/imgcache/?url=".urlencode($value["owner"]["profile_image"])."&size=100\");background-size: cover;background-position: 50% 50%;' class='result_image'></div><a href='".$value["link"]."' rel='nofollow'><div class='result_title'>".$value["title"]."</div></a><div class='result_description'>Asked by ".$value["owner"]["display_name"].".</div><div class='result_link'></div></div>";
+	}
 }
 
 ?>
