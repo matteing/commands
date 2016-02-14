ff749b0bc249157484fdce5cd632582d9d3b40d9
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-02-14 12:04:50 -0800

    tests

--- stack.php
+++ stack.php
@@ -7,7 +7,7 @@ curl_setopt($ch, CURLOPT_URL, "http://api.stackexchange.com/2.2/search?order=des
 $result = curl_exec($ch);
 curl_close($ch);
 
-echo "http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".urlencode($query[1])."&site=stackoverflow";
+echo "http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".$query[1]."&site=stackoverflow";
 $obj = json_decode($result);
 
 print_r($obj);
