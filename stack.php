7b4085140807fbf1f72fef796f628ebf0baebe51
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-02-14 11:59:06 -0800

    Tests

--- stack.php
+++ stack.php
@@ -3,6 +3,6 @@
 $json = file_get_contents("http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".urlencode($query[1])."&site=stackoverflow");
 $obj = json_decode($json);
 
-print_r($obj);
+print_r $obj;
 
 ?>
