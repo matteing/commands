03d74238c793c2f22a454889c6b3c573bf0658db
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-02-14 12:30:24 -0800

    error message

--- stack.php
+++ stack.php
@@ -23,14 +23,18 @@ function gzdecoder($d){
     return $u;
 }
 
-$result=gzdecoder(file_get_contents("http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".urlencode($query[1])."&site=stackoverflow"));
-$obj = json_decode($result,true);
-//--print_r($obj); //--uncomment for debug
-
-foreach ($obj["items"] as $value){
-	if ($value["owner"]["user_type"]=="registered"){
-		echo "<div class='result_case'><div style='background:url(\"https://twiicdn.com/imgcache/?url=".urlencode($value["owner"]["profile_image"])."&size=100\");background-size: cover;background-position: 50% 50%;' class='result_image'></div><a href='".$value["link"]."' rel='nofollow'><div class='result_title'>".$value["title"]."</div></a><div class='result_description'>Asked by ".$value["owner"]["display_name"].".</div><div class='result_link'></div></div>";
+if ($query[1]!=""){
+	$result=gzdecoder(file_get_contents("http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".urlencode($query[1])."&site=stackoverflow"));
+	$obj = json_decode($result,true);
+	//--print_r($obj); //--uncomment for debug
+	
+	foreach ($obj["items"] as $value){
+		if ($value["owner"]["user_type"]=="registered"){
+			echo "<div class='result_case'><div style='background:url(\"https://twiicdn.com/imgcache/?url=".urlencode($value["owner"]["profile_image"])."&size=100\");background-size: cover;background-position: 50% 50%;' class='result_image'></div><a href='".$value["link"]."' rel='nofollow'><div class='result_title'>".$value["title"]."</div></a><div class='result_description'>Asked by ".$value["owner"]["display_name"].".</div><div class='result_link'></div></div>";
+		}
 	}
+}else{
+	echo "<h2 style='text-align:center;'>We need more info</h2>We can search StackOverflow for you, but we need something to search for";
 }
 
 ?>
