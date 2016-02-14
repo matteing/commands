2f149dcc830becac6683237dbde2e8e777b70e2c
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-02-14 12:40:13 -0800

    sort fix

--- stack.php
+++ stack.php
@@ -24,7 +24,7 @@ function gzdecoder($d){
 }
 
 if ($query[1]!=""){
-	$result=gzdecoder(file_get_contents("http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".urlencode($query[1])."&site=stackoverflow"));
+	$result=gzdecoder(file_get_contents("http://api.stackexchange.com/2.2/search?order=desc&sort=relevance&intitle=".urlencode($query[1])."&site=stackoverflow"));
 	$obj = json_decode($result,true);
 	//--print_r($obj); //--uncomment for debug
 	
