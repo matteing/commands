632a94b9717f44063bcf7530e4e509ff43a704cf
Anthony Rossbach <anthonyrossbach@gmail.com>
2016-02-14 12:06:46 -0800

    tests

--- stack.php
+++ stack.php
@@ -3,7 +3,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, "http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".urlencode($query[1])."&site=stackoverflow");
+curl_setopt($ch, CURLOPT_URL, "http://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=".$query[1]."&site=stackoverflow");
 $result = curl_exec($ch);
 curl_close($ch);
 
